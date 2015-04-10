6b244f2e5b6345eae927a276cfaba18fce3c4474
世凡 黄 <773108248@qq.com>
2015-04-10 15:11:02 +0800

    add someskill & fix named

--- app/scripts/controllers/skill.js
+++ app/scripts/controllers/skill.js
@@ -12,11 +12,12 @@ angular.module('cvApp')
     $scope.items = [
       {key: 'Web开发', value: 'Java/Node(io.js)/Python'},
       {key: 'Web框架', value: 'struts2/hibernate/spring/spring mvc/express/koa/flask'},
-      {key: '前端框架', value: 'Bower/Grunt/Yo/Gulp'},
+      {key: '前端框架', value: 'Bootstrap/AngularJS/React/RequireJS/Browserify'},
+      {key: '前端工具', value: 'Bower/Grunt/Yo/Gulp/'},
       {key: '数据库相关', value: 'Mysql/MongoDB/SQLite'},
       {key: '版本管理', value: 'SVN/Git'},
       {key: '单元测试', value: 'JUnit/Karma'},
-      {key: '移动开发', value: 'Android/NDK/React Native/Hybrld App'},
+      {key: '移动开发', value: 'Android/NDK/React Native/Hybrld App/JQuery Mobile'},
       {key: '游戏开发', value: 'Cocos2dx/Phaser'},
     ];
   });
